f37786225b6b7dfffd0712cc52d0b737eb6504aa
cernej <cernej@email.cz>
2024-09-23 06:45:05 +0000

    first python example

---
+++ first.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# demonstration of using hidden variable __name__
+
+def say_hello():
+    print('Hello!')
+
+if __name__ == '__main__':
+    say_hello()
---
+++ second.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from first import say_hello
+
+if __name__ == '__main__':
+    say_hello()
