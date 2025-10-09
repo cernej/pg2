fef960de27c1b13ca7259a7331aaa7578e7e41ac
cernej <cernej@email.cz>
2025-10-09 12:31:50 +0000

    uloha2

---
+++ second.py
@@ -0,0 +1,10 @@
+def cislo_text(cislo):
+    # funkce zkonvertuje cislo do jeho textove reprezentace
+    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
+    return "dvacet pět"
+
+
+if __name__ == "__main__":
+    cislo = input("Zadej číslo: ")
+    text = cislo_text(int(cislo))
+    print(text)
