7a8ffdd13fcf54c809d4d8ecb3e6502696beb56c
cernej <cernej@email.cz>
2025-10-16 09:18:35 +0000

    prednaska 3

---
+++ prednaska3.py
@@ -0,0 +1,29 @@
+if __name__ == "__main__":
+
+    # a = 1
+    # while a < 20:
+    #     print(f"a je {a} a je mensi nez 20")
+    #     #a += 1
+
+    # print("dalsi operace")
+
+    # text = list("ahoj, jak se mas?")
+    # pozice_znaku_a = []
+
+    # for i, x in enumerate(text):
+    #     if x == "a":
+    #         #print(f"znak 'a' je {i}. znak v retezci")
+    #         pozice_znaku_a.append(i)
+    
+    # print(pozice_znaku_a)
+
+    # for i in pozice_znaku_a:
+    #     text[i] = "A"
+    
+    # print(text)
+
+    jmena = ["Alice", "Bob", "Eva"]
+    veky = [30, 25, 22]
+    zip(jmena, veky)
+    [["Alice", 30], ["Bob", 25], ["Eva", 22]]
+
