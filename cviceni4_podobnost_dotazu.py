206a0ee5cd18d0126c4ac97e0249170343e9a6a6
cernej <cernej@email.cz>
2025-10-23 09:16:09 +0000

    cviceni 4

--- cviceni4_podobnost_dotazu.py
+++ cviceni4_podobnost_dotazu.py
@@ -2,37 +2,28 @@ from cviceni4_jaccard import jaccardova_vzdalenost_mnozin
 from cviceni4_levenstein import levensteinova_vzdalenost
 
 
-def deduplikace_dotazu2(dotazy):
+def deduplikace_dotazu(dotazy):
     """
     tato funkce spocita jaccardovu vzdalenost a levensteinovu vzadelnost a vyradi z seznamu dotazy, polozky, ktere budou mit
     jaccardovu vzdalenost mensi nez 0.5 a levensteinovu vzdalenost <= 1
     """
-    results = [dotazy.pop(0)]
-    for dotaz1 in dotazy:
-        remove = False
-        for dotaz2 in results:
-            jaccard = jaccardova_vzdalenost_mnozin(dotaz1["serp"], dotaz2["serp"])
-            levenstein = levensteinova_vzdalenost(dotaz1["dotaz"], dotaz2["dotaz"])
-            if jaccard < 0.5 or levenstein <= 1:
-                remove = True
-        if not remove:
-            results.append(dotaz1)
-    return results
-
-
-def deduplikace_dotazu(dotazy):
     i = 0
     while i < len(dotazy):
         j = i + 1
         while j < len(dotazy):
             jaccard = jaccardova_vzdalenost_mnozin(dotazy[i]["serp"], dotazy[j]["serp"])
             levenstein = levensteinova_vzdalenost(dotazy[i]["dotaz"], dotazy[j]["dotaz"])
-            if jaccard < 0.5 or levenstein <= 1:
+            if jaccard < 0.5:
+                print(f"Remove query {dotazy[j]["dotaz"]} because of jaccard distance {jaccard}")
+                dotazy.pop(j)
+            elif levenstein <= 1:
+                print(f"Remove query {dotazy[j]["dotaz"]} because of levenstein distance {levenstein}")
                 dotazy.pop(j)
             else:
                 j += 1
         i += 1
     return dotazy
+
 
 
 if __name__ == "__main__":
