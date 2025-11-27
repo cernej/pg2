81cb947eaab08ee8c361e0f88aca7b34a9759915
cernej <cernej@email.cz>
2025-11-27 09:56:55 +0000

    cviceni8

--- cviceni8.py
+++ cviceni8.py
@@ -11,8 +11,46 @@ class Osoba:
     def __str__(self):
         return f'Osoba({self.jmeno}, {self.telefon}, {self.email})'
 
+    @property
+    def jmeno(self):
+        return self.__jmeno
+
+    @jmeno.setter
+    def jmeno(self, hodnota: str):
+        for c in hodnota:
+            if not c.isalpha() and not c.isspace():
+                raise InvalidData(f'Chybne zadane jmeno "{hodnota}"')
+        self.__jmeno = hodnota
+
+    @property
+    def telefon(self):
+        return self.__telefon
+    
+    @telefon.setter
+    def telefon(self, hodnota: str):
+        if not len(hodnota) == 13:
+            raise InvalidData(f'Cislo musi obsahovat 13 znaku')
+        if hodnota[0] != '+':
+            raise InvalidData(f'Cislo musi zacinat +')
+        if not hodnota[1:].isdigit():
+            raise InvalidData(f'Neni cislo')
+        self.__telefon = hodnota
+
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, hodnota: str):
+        if '@' not in hodnota:
+            raise InvalidData(f'Email musi obsahovat @')
+        if not hodnota.endswith('.cz'):
+            raise InvalidData(f'Email musi koncit .cz')
+        if not hodnota.replace('@', '').replace('.', '').isalnum():
+            raise InvalidData(f'Email obsahuje nepovolene znaky')
+        self.__email = hodnota
 
 if __name__ == "__main__":
-
-    o1 = Osoba("12345", "+420777888999", "ahoj@email.cz")
+    o1 = Osoba("Tomas", "+420777888999", "ahoj1@email.cz")
     print(o1)
+    
