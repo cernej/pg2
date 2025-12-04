532562f534ca73b4a15ea3d20d30d25dd89aeb2b
cernej <cernej@email.cz>
2025-12-04 09:38:40 +0000

    cvicieni9

--- cviceni9.py
+++ cviceni9.py
@@ -1,8 +1,10 @@
 def nejvetsi(seznam_cisel):
-    return 0
+    if not seznam_cisel:
+        return None
+    return max(seznam_cisel)
 
 
 def test_nejvetsi():
     assert nejvetsi([1,2,3,4,5]) == 5
     assert nejvetsi([100, 50, 30, 10]) == 100
-    assert nejvetsi() == None
+    assert nejvetsi([]) == None
